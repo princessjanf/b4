d74cc6a38a3e8b6f5f32ad36abeccacb2ff0857f
Gerald Immanuel <geraldhim97@gmail.com>
2017-03-15 16:22:35 +0700

    Interface Homepage
    Bikin UI Homepage

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -10,7 +10,7 @@ class MainController extends Controller
 {
     function index()
     {
-      return view('pages.welcome');
+      return view('pages.homepage');
     }
 
     function login()
--- resources/views/pages/example.blade.php
+++ resources/views/pages/example.blade.php
@@ -6,6 +6,14 @@
 	class="active"
 @endsection
 
+@section('nav.user')
+	<?php
+			echo '<ul>';
+			echo $user->username;
+			echo '</ul>';
+		?>
+@endsection
+
 @section('body')
 		<?php
 			echo '<ul>';
