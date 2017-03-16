19f9456883210f021d07afba3951e34a7cf7cbd6
princessjanf <princess.janf@gmail.com>
2017-03-16 16:32:44 +0700

    Commit
    commit

--- resources/views/pages/createScholarship.blade.php
+++ resources/views/pages/createScholarship.blade.php
@@ -106,6 +106,16 @@ function insertRow(){
 			return false;
 			//show alert?
 	}
+	$(function () {
+  $('#createScholarshipForm').parsley().on('field:validated', function() {
+    var ok = $('.parsley-error').length === 0;
+    $('.bs-callout-info').toggleClass('hidden', !ok);
+    $('.bs-callout-warning').toggleClass('hidden', ok);
+  })
+  .on('form:submit', function() {
+    return false; // Don't submit form for this demo
+  });
+});
 
 
 </script>
