2fcd9a8f73880c91b6b49452fca2d907244885ee
princessjanf <princess.janf@gmail.com>
2017-05-19 13:25:51 +0700

    Persebaran beasiswa jenjang
    Persebaran beasiswa per jenjang.
    Persebaran beasiswa jenjang tertentu utk semua fakultas dan semua prodi

--- app/Http/Controllers/ChartController.php
+++ app/Http/Controllers/ChartController.php
@@ -57,37 +57,73 @@ class ChartController extends Controller
 
       $jenjang = DB::table('beasiswa_jenjang_prodi')->join('jenjang','beasiswa_jenjang_prodi.id_jenjang','=','jenjang.id_jenjang')->select('beasiswa_jenjang_prodi.id_jenjang', 'nama_jenjang')->distinct()->get();
 
+
+      $namaProdi = [];
+      $jumlahBeasiswa = [];
+      $arrJenjang = [];
+      $jjg = [];
       foreach($jenjang as $index=>$j)
       {
-        $namaProdi = [];
-        $jumlahBeasiswa = [];
+        array_push($jjg, $j->nama_jenjang);
         $jumlahProdi = DB::table('beasiswa_jenjang_prodi')->where('id_jenjang', $j->id_jenjang)->join('program_studi', 'program_studi.id_prodi','=','beasiswa_jenjang_prodi.id_prodi')->select('beasiswa_jenjang_prodi.id_prodi', 'nama_prodi')->distinct()->get();
         foreach ($jumlahProdi as $jp)
         {
           $jml = DB::table('beasiswa_jenjang_prodi')->where('id_prodi', $jp->id_prodi)->where('id_jenjang', $j->id_jenjang)->count();
           array_push($namaProdi, $jp->nama_prodi);
           array_push($jumlahBeasiswa, $jml);
+          array_push($arrJenjang, $j->nama_jenjang);
+
         }
 
-        ${'chart'.$index}= Charts::create('pie', 'highcharts')->title('Beasiswa '.$j->nama_jenjang)
+        ${'prodi'.$index}= Charts::create('pie', 'highcharts')->title('Persebaran Beasiswa '.$j->nama_jenjang)
                               ->labels($namaProdi)->values($jumlahBeasiswa);
+
+
+
+        $namaFakultas = [];
+        $jmlBeasiswaFakultas = [];
+        $jmlFakultas = DB::table('beasiswa_jenjang_prodi as bjp')->where('bjp.id_jenjang', $j->id_jenjang)
+                      ->join('jenjang_prodi as jp', 'bjp.id_jenjang', '=', 'jp.id_jenjang')
+                      ->join('program_studi as ps', 'ps.id_prodi', '=', 'bjp.id_prodi')
+                      ->join('fakultas as f', 'f.id_fakultas', '=', 'ps.id_fakultas')
+                      ->select('nama_fakultas', 'f.id_fakultas')->distinct()->get();
+        // return $jmlFakultas;
+        foreach($jmlFakultas as $jf)
+        {
+          $jml = DB::table('beasiswa_jenjang_prodi as bjp')->join('jenjang_prodi as jp', 'bjp.id_jenjang', '=', 'jp.id_jenjang')
+                        ->join('program_studi as ps', 'ps.id_prodi', '=', 'bjp.id_prodi')
+                        ->join('fakultas as f', 'f.id_fakultas', '=', 'ps.id_fakultas')
+                        ->where('f.id_fakultas', $jf->id_fakultas)->where('bjp.id_jenjang', $j->id_jenjang)->select('id_beasiswa')->distinct()->get();
+
+          array_push($namaFakultas, $jf->nama_fakultas);
+          array_push($jmlBeasiswaFakultas, count($jml));
+        }
+        ${'fak'.$index}= Charts::create('pie', 'highcharts')->title('Persebaran Beasiswa '.$j->nama_jenjang)
+                              ->labels($namaFakultas)->values($jmlBeasiswaFakultas);
+
 
       }
       $chart =
        Charts::multidatabase('bar', 'highcharts')
-              ->title("Persebaran Beasiswa Per Prodi")
+              ->title("Persebaran Beasiswa Per Jenjang")
               ->elementLabel('Jumlah Beasiswa')
-              ->dataset('Jumlah Beasiswa Per Program Studi', DB::table('beasiswa_jenjang_prodi as bjp')->join('program_studi as ps', 'bjp.id_prodi','=','ps.id_prodi')
-                          ->join('fakultas as f', 'f.id_fakultas', '=', 'ps.id_fakultas')->get())
-              ->groupBy('nama_fakultas');
+              ->dataset('Jumlah Beasiswa', DB::table('beasiswa_jenjang_prodi as bjp')->join('program_studi as ps', 'bjp.id_prodi','=','ps.id_prodi')
+                          ->join('jenjang as j', 'j.id_jenjang', '=', 'bjp.id_jenjang')->get())
+              ->groupBy('nama_jenjang');
 
       $toreturn = ['user'=>$user, 'chart'=>$chart, 'pengguna'=>$pengguna, 'namarole'=>$namarole, 'countjenjang'=>count($jenjang)];
+      $toreturn['namaprodi'] = $namaProdi;
+      $toreturn['jumlahbeasiswa'] = $jumlahBeasiswa;
+      $toreturn['arrjenjang'] = $arrJenjang;
+      $toreturn['jenjang'] = $jjg;
 
       // $toreturn = ['namarole'=>$chk];
       //compact('user','pengguna','namarole','chart0');
       for ($i = 0; $i<count($jenjang); $i++)
       {
-        $var='chart'.$i;
+        $var='prodi'.$i;
+        $toreturn[$var] = $$var;
+        $var='fak'.$i;
         $toreturn[$var] = $$var;
 
       }
@@ -112,7 +148,7 @@ class ChartController extends Controller
       $namarole = ChartController::getNamarole($pengguna);
 
        $chart = Charts::multidatabase('bar', 'highcharts')
-                              ->title("Jumlah Beasiswa Per Fakultas")    
+                              ->title("Jumlah Beasiswa Per Fakultas")
                               ->elementLabel('Jumlah')
                               ->dataset('Beasiswa', DB::table('beasiswa')
                                 ->join('beasiswa_jenjang_prodi', 'beasiswa_jenjang_prodi.id_beasiswa','=','beasiswa.id_beasiswa')
@@ -129,7 +165,7 @@ class ChartController extends Controller
       $namarole = ChartController::getNamarole($pengguna);
 
        $chart = Charts::multidatabase('bar', 'highcharts')
-                              ->title("Jumlah Beasiswa Per Program Studi")    
+                              ->title("Jumlah Beasiswa Per Program Studi")
                               ->elementLabel('Jumlah')
                               ->dataset('Beasiswa', DB::table('beasiswa')
                                 ->join('beasiswa_jenjang_prodi', 'beasiswa_jenjang_prodi.id_beasiswa','=','beasiswa.id_beasiswa')
@@ -165,12 +201,12 @@ class ChartController extends Controller
 // return var_dump(DB::table('pendaftaran_beasiswa as pb')->join('mahasiswa as m', 'm.id_user', '=', 'pb.id_mahasiswa')->join('fakultas as f', 'f.id_fakultas', '=', 'm.id_fakultas')->join('beasiswa', 'beasiswa.id_beasiswa', '=', 'pb.id_beasiswa')->get());
        $chart = array();
        array_push($chart, Charts::multidatabase('bar', 'highcharts')
-                              ->title("Jumlah Pendaftar Per Fakultas")    
+                              ->title("Jumlah Pendaftar Per Fakultas")
                               ->elementLabel('Jumlah')
                               ->dataset('Pendaftar', DB::table('pendaftaran_beasiswa as pb')->join('mahasiswa as m', 'm.id_user', '=', 'pb.id_mahasiswa')->join('fakultas as f', 'f.id_fakultas', '=', 'm.id_fakultas')->join('beasiswa', 'beasiswa.id_beasiswa', '=', 'pb.id_beasiswa')->get())
                               ->groupBy('nama_fakultas')
                             );
-      
+
 
       return view('pages.statistik-beasiswa3', compact('user','pengguna','namarole','chart'))->withUser($user)->withPengguna($pengguna)->withNamarole($namarole)->withBeasiswa($beasiswa);
     }
